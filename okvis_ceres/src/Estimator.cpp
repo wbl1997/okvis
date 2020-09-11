c6a41a6054c16d4590b71fd1bfc9ee61ece03a96
JIanzhu Huai <huai.3@osu.edu>
2020-09-11 11:57:24 +0800

    initialize gyro bias with input

--- okvis_ceres/src/Estimator.cpp
+++ okvis_ceres/src/Estimator.cpp
@@ -154,7 +154,8 @@ bool Estimator::addStates(
     }
     speedAndBias.setZero();
     speedAndBias.head<3>() = pvstd_.v_WS;
-    speedAndBias.segment<3>(6) = imuParametersVec_.at(0).a0;
+    speedAndBias.segment<3>(3) = imuParametersVec_.at(0).g0;
+    speedAndBias.tail<3>() = imuParametersVec_.at(0).a0;
   } else {
     // get the previous states
     uint64_t T_WS_id = statesMap_.rbegin()->second.id;
